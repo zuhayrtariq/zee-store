fb32c4583ba46c5fbc5cb365e13169b385383846
Zuhayr <zuhayrtariq4@gmail.com>
2025-01-29 12:46:18 +0500

    updated ts rule in eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,11 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  }),
 ];
 
 export default eslintConfig;
